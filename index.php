ea476086294ec8ba9f086453402a07fff951be69
Rob Hardy <robhogg68@googlemail.com>
2013-01-28 02:55:22 +0100

    Moved code to display tweets into separate file

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-	require("tweets.php");
+	require("twitlib.php");
+   $Index = true;	
+	$archname = (isset($_GET['archive']))?$_GET['archive']:"";
+
+	$to = 50;
+	$from = 0;
+	$order = "date-";
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,22 +14,8 @@
 <title>EDMOOC</title>
 </head>
 <body>
-<?php
-	$json = file_get_contents("search4.json");
-	$json = json_decode("$json");
-	foreach($json->results as $key => $result) {
-		echo "<h2>".$key."</h1>\n";
-		echo '<li><a href="http://twitter.com/'.$result->from_user
-			.'">'.$result->from_user."</a></li>";
-		echo '<ul><li>'.$result->text.'</li>';
-		echo '<li><a href="http://twitter.com/'.$result->from_user
-			.'/status/'.$result->id_str.'">'.$result->created_at
-			.'</a></li>';
-		echo '<li>'.tweet_date_format($result->created_at).'</li></ul>';
-	   echo '</ul>';
-		var_dump($result);
-		echo "<br /><br />";
-	}
-?>
+	<div id="tweet-list">
+		<?php include("tw_list.php"); ?>
+	</div>
 </body>
 </html>
