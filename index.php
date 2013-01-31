45cd9174d27b60318b51b659965371653c99b06b
Rob Hardy <robhogg68@googlemail.com>
2013-01-31 01:46:22 +0100

    Tidied parameters

--- index.php
+++ index.php
@@ -3,19 +3,35 @@
    $Index = true;	
 	$archname = (isset($_GET['archive']))?$_GET['archive']:"";
 
-	$to = 50;
-	$from = 0;
-	$order = "date-";
+	$page = (isset($_GET['page']))?$_GET['page']:1;
+	$per_page = (isset($_GET['perpage']))?$_GET['perpage']:25;
+	if(isset($_GET['order']) && isset($_GET['dir'])
+		&& $_GET['dir'] = "desc") {
+		$order = $_GET['order'].'-';
+	} elseif(isset($_GET['order'])) {
+		$order = $_GET['order']."+";
+	} else {
+		$order = "date-";
+	}
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <title>EDMOOC</title>
+
+<base href="/twive">
+
+<link rel="stylesheet" href="/twive/tw_style.css">
 </head>
 <body>
-	<div id="tweet-list">
-		<?php include("tw_list.php"); ?>
+	<div id="header">
+	<?php get_header($archname); ?>
+	</div>
+	<div id="content">
+		<?php
+			include("tw_list.php"); ?>
 	</div>
 </body>
 </html>
