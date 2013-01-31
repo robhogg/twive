a6541c62cd4af064e0ac61114ee0f91e80f24b1f
Rob Hardy <robhogg68@googlemail.com>
2013-01-31 01:44:24 +0100

    Tidied parameters, few other changes

--- tw_list.php
+++ tw_list.php
@@ -7,9 +7,8 @@
 		*/
 		$archname = $_GET['archive'];
 		
-		$num = (isset($_GET['num']))?$_GET['num']:15;
-		$to = (isset($_GET['to']))?$_GET['to']:0;
-		$from = (isset($_GET['from']))?$_GET['from']:0;
+		$per_page = (isset($_GET['perpage']))?$_GET['perpage']:25;
+		$page = (isset($_GET['page']))?$_GET['page']:1;
 		if(isset($_GET['order']) && isset($_GET['dir']) 
 			&& $_GET['dir'] = "desc") {
 			$order = $_GET['order'].'-';
@@ -26,16 +25,26 @@
 		exit(0);
 	}
 
-echo("$archname, $to, $from, $order<br />");
 	// TODO: search criteria
+	$crit = array();
 
-   $tweets = get_tweets($archname,$to,$from,$order);
-	
-	$count = 0;
-	foreach($tweets as $tweet) {
-		echo("<div id=\"tweet-$count\" class=\"tweet\">\n");
-			echo(format_tweet($tweet));
-		echo("</div>");
-		$count += 1;
-	}
+   $tweets = get_tweets($archname,$per_page,($page - 1) * $per_page,
+		$order,$crit);
+	$tw_num = get_num_tweets($archname,$crit);
+
+	?>
+	<div id="list-controls">
+		<?php get_controls($page,ceil($tw_num / $per_page)); ?>
+	</div>
+	<div id="tweet-list">
+		<?php
+		$count = 0;
+		foreach($tweets as $tweet) {
+			echo("<div id=\"tweet-$count\" class=\"tweet\">\n");
+				echo(format_tweet($tweet));
+			echo("</div>");
+			$count += 1;
+		}
+		?>
+	</div>
 ?>
