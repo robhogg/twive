06286b3579d4e643ffdfa441408b9c844426ba6f
Rob Hardy <robhogg68@googlemail.com>
2013-02-03 11:46:10 +0100

    Added GPL v.3 licence gubbins.

--- config.php
+++ config.php
@@ -1,4 +1,23 @@
 <?php
+   */
+   * Copyright Rob Hardy 2013 (https://github.com/robhogg/twive)
+   *
+   * This file is part of Twive
+   * 
+   * Twive is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * (at your option) any later version.
+   * 
+   * Twive is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   * 
+   * You should have received a copy of the GNU General Public License
+   * along with Twive.  If not, see <http://www.gnu.org/licenses/>.
+   */
+
 	define("DB_NAME","dbname");
 	define("DB_HOST","dbhost");
 	define("DB_USER","dbuser");
