1a7e14d3f13707f2ad95e4512ac825d5fd6caed4
Rob Hardy <robhogg68@googlemail.com>
2013-02-03 10:50:47 +0100

    Linked to jquery

--- index.php
+++ index.php
@@ -15,6 +15,16 @@
 <base href="/twive">
 
 <link rel="stylesheet" href="/twive/tw_style.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js">
+</script>
+<script type="text/javascript">
+/*$(document).ready(function () {
+	$(".chart_bar").bind("click", function() {
+		$(this).css("background-color","red");
+		alert($(this).attr("id") + ": " +$(this).css("height"));
+	});
+});*/
+</script>
 </head>
 <body>
 	<div id="header">
