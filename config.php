1538e315ab9211c62ffb113bf912642fe3b67106
Rob Hardy <robhogg68@googlemail.com>
2013-02-03 11:19:43 +0100

    Moved clean config file to working directory.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	define("DB_NAME","dbname");
+	define("DB_HOST","dbhost");
+	define("DB_USER","dbuser");
+	define("DB_PASS","dbpass");
+
+	date_default_timezone_set("UTC");
+?>
--- tw_lib.php
+++ tw_lib.php
@@ -1,5 +1,5 @@
 <?php
-	require("../../main_scripts/config.php");
+	require("config.php");
 
 	$conn = @new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
